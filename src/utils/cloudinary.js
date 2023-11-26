520723ed50c482302b98e4a6fe68d85bce4e5b36
SAGAR KUMAR JHA <jha8768@gmail.com>
2023-11-27 00:02:04 +0530

    Register contCroller logic changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,7 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been uploaded successfully
-    console.log("file is uploaded on cloudinary ", response.url);
+    //unlink the local file
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
